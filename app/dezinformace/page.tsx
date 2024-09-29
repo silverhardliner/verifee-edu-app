e1e6b4c2a2dd737b3092bd3886f686d52cd76f93
Matej Kopecky <kopecm28@fel.cvut.cz>
2024-09-29 20:30:55 +0200

    initial commit

---
+++ app/dezinformace/page.tsx
@@ -0,0 +1,108 @@
+import { Lexend_Exa } from "next/font/google";
+import img_a from "@/public/images/1.0.a.PNG";
+import img_b from "@/public/images/1.0.b.PNG";
+import img_c from "@/public/images/1.0.c.PNG";
+import "./styles.css";
+import RowImage from "../ui/dezinformace/row-image";
+
+const lexend_exa = Lexend_Exa({ subsets: ["latin"], display: "swap" });
+
+export default function Page() {
+  const image_a = {
+    img: img_a,
+    alt: "img_a",
+    title: "MISINFORMACE",
+    detail:
+      "Míša od kamaráda slyšel, že film je založen na skutečných událostech. Ve skutečnosti to tak ale není a film je čistou fikcí. Míša to ale neví a tuto informaci nadšeně sdílí s Miladou bez špatného úmyslu.",
+    bubbleText:
+      "Slyšel jsem od kamaráda, že je film založený na skutečných událostech.",
+  };
+
+  const image_b = {
+    img: img_b,
+    alt: "img_b",
+    title: "DEZINFORMACE",
+    detail:
+      "Mája ví, že film není založený na skutečných událostech. Kdyby totiž skutečně zkontrolovala film jak tvrdí, tuto informaci by nenašla. Záměrně tím tedy zavádí a vědomě šíří lež.",
+    bubbleText:
+      "Vím, že je film podle skutečné události, byla jsem si to zkontrolovat na internetu.",
+  };
+
+  const image_c = {
+    img: img_c,
+    alt: "img_c",
+    title: "MALINFORMACE",
+    detail:
+      "Marek má přístup k zákulisním detailům, které jsou sice pravdivé, ale jejich šíření a zvěřejňování by mohlo poškodit jak herce, tak produkci. Přesto neváhá tyto informace sdílet s někým dalším. Říká tak sice pravdu, ale jeho úmyslem je uškodit druhému.",
+    bubbleText:
+      "Byl jsem u natáčení toho filmu a mám nejen nelichotivou fotku hlavní herečky, ale ještě k tomu nějaké nahrávky z produkce. Ukážu ti to!",
+  };
+
+  const images = [image_a, image_b, image_c];
+
+  return (
+    <div>
+      <div className={`text-5xl mb-4 ${lexend_exa.className}`}>
+        Dezinformace
+      </div>
+      <p>
+        Dezinformace jsou jedním ze tří typů Informačních poruch. Informačními
+        poruchami nazýváme informační chaos, který vzniká šířením nepravdivých
+        nebo zkreslených zpráv. Informační poruchy dělíme do tří kategorií podle
+        pravdivosti a záměru, se kterým jsou šířeny:
+      </p>
+      <ul className="list-disc pl-16">
+        <li>
+          <b>Dezinformace: </b>nepravdivé zprávy šířené se zlým záměrem
+          <div>(deepfakes, konspirační teorie, klasické fake news)</div>
+        </li>
+        <li>
+          <b>Misinformace: </b>nepravdivé zprávy šířené bez zlého záměru
+          <div>
+            (často vznikají nezáměrnou chybou, třeba chybným datem či výkladem
+            dat nebo nepochopením satiry)
+          </div>
+        </li>
+        <li>
+          <b>Malinformace: </b>pravdivé zprávy šířené se zlým záměrem
+          <div>
+            (často osobní info, klasicky sdílení cizích fotografií nebo trapných
+            videí, ale i jiné)
+          </div>
+        </li>
+      </ul>
+      <p>
+        Podívej se na následující scénář. Tři přátelé, Míša, Mája a Marek,
+        povídají své kamarádce Miladě o novém filmu, který právě vyšel.
+        Kliknutím na jeden z pojmů si projdi, jak by jednotlivé informační
+        poruchy mohly vypadat ve skutečném světě. Až budeš chtít, kliknutím na
+        tlačítko Pokračovat se posuň v úkolu dál.
+      </p>
+      <div className="border-solid border rounded-lg border-black my-10 flex w-full p-4">
+        {images.map((image, id) => {
+          return <RowImage content={image} key={id} />;
+        })}
+      </div>
+      <p>
+        Ačkoli se často setkáte se všemi třemi typy informačních poruch, nás
+        zajímají právě dezinformace, tedy lživé zprávy šířené se zlým záměrem.
+        Takové zprávy přirozeně existovaly vždy - s rozšířením internetu jsou
+        však stále častější a mají větší dopad.
+      </p>
+      <p>
+        Dezinformace jsou mocným nástrojem - dokáží šířit nepravdy, ale také
+        zasívat chaos, ve kterém zpochybňujeme vše, protože nevíme, čemu můžeme
+        věřit. Mohou je šířit jednotlivci, často však přichází od nepřátelských
+        států v rámci hybridní informační války. Dezinformátoři cíleně zatajují
+        nebo falšují původ informací, které uvádí, protože tyto informace buď
+        sami vymýšlí, nebo zkreslují tak, aby podporovaly jejich příběh.{" "}
+      </p>
+      <p>
+        Podrobnější informace o tom, kdo dezinformace šíří a za jakým účelem a
+        jak se jim bránit vám představí úkoly v této části cvičení. Nyní vám
+        představíme několik základních pojmů, které s dezinformacemi souvisí a
+        možná jste se s nimi již setkali.
+      </p>
+    </div>
+  );
+}
---
+++ app/ui/dezinformace/row-image.tsx
@@ -0,0 +1,31 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import bubble from "@/public/images/mluvena.PNG";
+
+export default function RowImage({
+  content,
+}: {
+  content: {
+    title: string;
+    img: StaticImport;
+    alt: string;
+    detail: string;
+    bubbleText: string;
+  };
+}) {
+  return (
+    <div className="flex-1 relative object-contain flex flex-col items-center px-4">
+      <p className="text-center text-xl">{content.title}</p>
+      <div className="relative w-full h-44">
+        <Image src={bubble} alt={content.alt} fill />
+        <div className="absolute inset-14 flex items-center justify-center">
+          <p className="text-center text-sm text-black">{content.bubbleText}</p>
+        </div>
+      </div>
+      <div className="relative w-full h-96 mt-[-80px]">
+        <Image src={content.img} alt={content.alt} fill />
+      </div>
+      <p className="text-center text-lg italic">{content.detail}</p>
+    </div>
+  );
+}
