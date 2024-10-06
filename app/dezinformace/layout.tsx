1db0f9d70c6d2d809dbd45b3dede5e0ed7082ca0
Matej Kopecky <kopecm28@fel.cvut.cz>
2024-10-06 23:26:36 +0200

    improved padding for mobile

--- app/dezinformace/layout.tsx
+++ app/dezinformace/layout.tsx
@@ -1,10 +1,10 @@
 import "./styles.css"
-import {Kumbh_Sans} from "next/font/google"
+import { Kumbh_Sans } from "next/font/google"
 import { Open_Sans } from "next/font/google";
 
 const kumbh_sans = Kumbh_Sans({
-    subsets: ['latin'],
-    display: 'swap'
+  subsets: ['latin'],
+  display: 'swap'
 })
 
 const open_sans = Open_Sans({
@@ -12,13 +12,12 @@ const open_sans = Open_Sans({
   display: 'swap'
 })
 
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={kumbh_sans.className}>
-      <div className={`header-footer mb-7 ${open_sans.className}`}>VERIFEE EDUCATION</div>
-      <div className="px-36">{children}</div>
-      <div className={`header-footer mt-7 ${open_sans.className}`}>VERIFEE EDUCATION</div>
-    </div>
+  <div className={kumbh_sans.className}>
+    <div className={`header-footer mb-7 ${open_sans.className}`}>VERIFEE EDUCATION</div>
+    <div className="px-4 md:px-36">{children}</div>
+    <div className={`header-footer mt-7 ${open_sans.className}`}>VERIFEE EDUCATION</div>
+  </div>
   );
 }
