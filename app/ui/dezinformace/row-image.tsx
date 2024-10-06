98c470af8cfb421902a17b0a7bc398f82a63d151
Matej Kopecky <kopecm28@fel.cvut.cz>
2024-10-06 23:45:21 +0200

    improved width of images for screens

--- app/ui/dezinformace/row-image.tsx
+++ app/ui/dezinformace/row-image.tsx
@@ -14,15 +14,15 @@ export default function RowImage({
   };
 }) {
   return (
-    <div className="flex-1 relative object-contain flex flex-col items-center px-4">
+    <div className="flex-1 relative object-contain flex flex-col items-center sm:px-4">
       <p className="text-center text-xl">{content.title}</p>
-      <div className="relative w-full sm:w-2/3 md:w-1/2 lg:w-1/3 h-44">
+      <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 h-44">
         <Image src={bubble} alt={content.alt} fill />
         <div className="absolute inset-14 flex items-center justify-center">
           <p className="text-center text-sm text-black">{content.bubbleText}</p>
         </div>
       </div>
-      <div className="relative w-full sm:w-2/3 md:w-1/2 lg:w-1/3 h-96 mt-[-80px]">
+      <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 h-96 mt-[-80px]">
         <Image src={content.img} alt={content.alt} fill />
       </div>
       <p className="text-center text-lg italic">{content.detail}</p>
